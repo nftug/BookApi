85400d446beed13490be933e296875d9aadbc5c1
V-nyang <whitepony1214@gmail.com>
2024-03-15 21:25:42 +0900

    Implemented use cases for Publisher

---
+++ BookApi.UseCase/Publishers/UpdatePublisher.cs
@@ -0,0 +1,26 @@
+using BookApi.Domain.DTOs.Commands;
+using BookApi.Domain.Exceptions;
+using BookApi.Domain.Interfaces;
+using BookApi.Domain.Services;
+using BookApi.Domain.ValueObjects.Shared;
+using MediatR;
+
+namespace BookApi.UseCase.Publishers;
+
+public class UpdatePublisher
+{
+    public record Command(ActorForPermission Actor, int PublisherID, PublisherCommandDTO CommandForm) : IRequest;
+
+    public class Handler(PublisherSaveService publisherSaveService, IPublisherRepository PublisherRepository) : IRequestHandler<Command>
+    {
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var publisher =
+                await PublisherRepository.FindAsync(request.Actor, request.PublisherID)
+                ?? throw new ItemNotFoundException();
+
+            var permission = new AdminOnlyPermission(request.Actor);
+            await publisherSaveService.UpdateAsync(permission, publisher, request.CommandForm.Name);
+        }
+    }
+}
