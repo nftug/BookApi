528c4a70cde4ec615765106a9d4082f912668587
V-nyang <whitepony1214@gmail.com>
2024-03-16 01:46:28 +0900

    Fixed queries of publisher and author

--- BookApi.Domain/DTOs/Responses/PublisherResponseDTO.cs
+++ BookApi.Domain/DTOs/Responses/PublisherResponseDTO.cs
@@ -3,6 +3,8 @@ namespace BookApi.Domain.DTOs.Responses;
 public record PublisherResponseDTO(
     int ID,
     string Name,
-    IEnumerable<BookSummaryResponseDTO> Books,
-    IEnumerable<ItemSummaryResponseDTO> RelatedAuthors
-);
+    IEnumerable<BookSummaryResponseDTO> Books
+)
+{
+    public IEnumerable<ItemSummaryResponseDTO> RelatedAuthors { get; init; } = null!;
+}
