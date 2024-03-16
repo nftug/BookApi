d6cf12dbb822b3c264168c5012f8de83a36324ab
V-nyang <whitepony1214@gmail.com>
2024-03-16 17:58:20 +0900

    Added UseCaseTestBase

---
+++ BookApi.Test/Abstractions/UseCaseTestBase.cs
@@ -0,0 +1,53 @@
+using BookApi.Domain;
+using BookApi.Infrastructure;
+using BookApi.UseCase.Books;
+using MediatR;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookApi.Test.Abstractions;
+
+public abstract class UseCaseTestBase : IDisposable
+{
+    private readonly SqliteConnection _connection;
+    protected readonly IServiceProvider ServiceProvider;
+
+    protected ISender Mediatr => ServiceProvider.GetRequiredService<ISender>();
+    protected BookDbContext DbContext => ServiceProvider.GetRequiredService<BookDbContext>();
+
+    // setup
+    public UseCaseTestBase()
+    {
+        // テストケースごとにSQLiteのインメモリDBを作成する
+        _connection = new SqliteConnection();
+        _connection.Open();
+
+        // DIコンテナの設定
+        ServiceProvider =
+            new ServiceCollection()
+                .AddDbContext<BookDbContext>(
+                    opt => opt
+                        .UseSqlite("DataSource=:memory:")
+                        .UseLazyLoadingProxies()
+                )
+                .AddDomainServices()
+                .AddInfrastructureServices()
+                .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetBook).Assembly))
+                .BuildServiceProvider();
+
+        // DBの初期化
+        using var temporaryScope = ServiceProvider.CreateScope();
+        var dbContext = temporaryScope.ServiceProvider.GetRequiredService<BookDbContext>();
+        dbContext.Database.EnsureCreated();
+
+        // ここに共通のシード処理を書く
+    }
+
+    // teardown
+    public void Dispose()
+    {
+        // 接続が解除されるとインメモリDBも破棄される
+        _connection.Dispose();
+    }
+}
